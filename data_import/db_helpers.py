a12f3a979b2686570ec1da7736ddef6718e6e7a0
Nicolas Noé <nicolas@niconoe.org>
2014-04-30 14:20:48 +0200

    Created helper to import CSV file to table.

--- data_import/db_helpers.py
+++ data_import/db_helpers.py
@@ -1,4 +1,11 @@
+import psycopg2
+import csv
+
 from utils import exec_shell
+
+# TODO: Currently, this only works when Postgres user doesn't need to provide a password...
+# (Do this by using only psycopg2 instead of PG binaries - createdb, psql, ...) ?
+# We should also support setting a port number !!
 
 
 def _make_db_args(params):
@@ -9,6 +16,24 @@ def _make_db_args(params):
 
 def _make_db_args_name(params):
     return _make_db_args(params) + " " + params['name']
+
+
+class DBConnection(object):
+    def __init__(self, params):
+        self.connection = psycopg2.connect(database=params['name'],
+                                           user=params['username'],
+                                           host=params['host'])
+
+    def __enter__(self):
+        return self.connection
+
+    def __exit__(self, type, value, traceback):
+        self.connection.close()
+
+
+def _connect(params):
+    """Connect to the database and return the connexion object."""
+    return psycopg2.connect(database=params['name'], user=params['username'], host=params['host'])
 
 
 def check_db_existence(params):
@@ -37,5 +62,26 @@ def load_sqlfile(filename, db_params):
     return exec_shell(cmd)
 
 
-def copy_csvfile_to_table(file, table_name, db_params):
-    pass
+def copy_csvfile_to_table(f, table_name, db_params):
+    with DBConnection(db_params) as conn:
+        cur = conn.cursor()
+
+        # cur.copy_from is shitty() (quotes, error messages, ...)
+        # so we have to reinvent the wheel
+        # UGLY UGLY UGLY (but works)
+        input_file = csv.DictReader(f)
+        for row in input_file:
+            fields_list, values_list = [], []
+            for k, v in row.iteritems():
+                fields_list.append('"' + k + '"')
+                values_list.append(v)
+
+            placeholders = ""
+            for i, val in enumerate(fields_list):
+                placeholders += "%s"
+                if i != len(fields_list) - 1:
+                    placeholders += ','
+
+            s = "INSERT INTO " + table_name + " ({fields})".format(fields=','.join(fields_list)) + " VALUES (" + placeholders + ")"
+            cur.execute(s, tuple(values_list))
+            conn.commit()
--- data_import/import_all.py
+++ data_import/import_all.py
@@ -2,10 +2,11 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import os
 
 from prompter import yesno
 
-from db_helpers import check_db_existence, drop_database, create_database, load_sqlfile
+from db_helpers import check_db_existence, drop_database, create_database, load_sqlfile, copy_csvfile_to_table
 from utils import make_action_or_exit
 
 # Credentials should allow creation/deletion of databases
@@ -28,6 +29,9 @@ def main(prog_args):
 
     make_action_or_exit("Creating database...", o, create_database, DB_CONF)
     make_action_or_exit("Creating input schema...", o, load_sqlfile, '00_input_schema.sql', DB_CONF)
+
+    fn = os.path.join(os.path.dirname(__file__), './ifbl_csv_source/IFBL2009.csv')
+    copy_csvfile_to_table(open(fn), 'ifbl_2009', DB_CONF)
 
 
 if __name__ == "__main__":
