c55e7c880d772c54a3753b4cbc1f778e9998f605
Nicolas Noé <nicolas@niconoe.org>
2014-10-14 15:54:23 +0200

    Website: text/config updates.

--- webapp/config/environment.rb
+++ webapp/config/environment.rb
@@ -7,6 +7,6 @@ Ifblserver::Application.initialize!
 # Overriden in production.rb
 Rails.configuration.destination_emails = {
   :technical => ['n.noe@biodiversity.be'],
-  :scientific => ['niconoe@gmail.com', 'nicolas@niconoe.org']
+  :scientific => ['Dimitri.BROSENS@INBO.BE', 'wouter.vanlanduyt@inbo.be']
 }
 
